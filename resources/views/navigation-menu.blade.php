ae31ed416723a3025eb58ef9bda74d96f1726e18
rohitgvariya <vennerinfotech@gmail.com>
2025-07-22 18:54:41 +0530

    Remove Light and Dark Theme icon

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -41,11 +41,11 @@
           @if (in_array('Order', restaurant_modules()) && user_can('Show Order') && restaurant()->hide_new_orders == 0)
             @livewire('dashboard.todayOrders')
             @endif
-            
+
             @if (in_array('Reservation', restaurant_modules()) && user_can('Show Reservation') && restaurant()->hide_new_reservations == 0 && in_array('Table Reservation', restaurant_modules()))
             @livewire('dashboard.todayReservations')
             @endif
-            
+
             @if (in_array('Waiter Request', restaurant_modules()) && user_can('Manage Waiter Request') && restaurant()->hide_new_waiter_request == 0)
             @livewire('dashboard.activeWaiterRequests')
             @livewire('dashboard.runningOrders')
@@ -110,7 +110,7 @@
           <div class="tooltip-arrow" data-popper-arrow></div>
         </div>
 
-        <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
+        {{-- <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
           class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
           <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg">
@@ -122,7 +122,7 @@
               d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
               fill-rule="evenodd" clip-rule="evenodd"></path>
           </svg>
-        </button>
+        </button> --}}
 
         <div id="tooltip-toggle" role="tooltip"
           class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
